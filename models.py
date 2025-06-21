fceff1196c234d7179ae45ae116727439b8db81b
ILPIV <piv10piv@mail.ru>
2025-06-21 16:17:44 +0300

    init_db

--- models.py
+++ models.py
@@ -2,15 +2,11 @@ from sqlalchemy import ForeignKey, String, BigInteger
 from sqlalchemy.orm import Mapped, DeclarativeBase, mapped_column
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
 
-
 engine = create_async_engine(url='sqlite+aiosqlite:///db.sqlite3', echo=True)
-
 async_session = async_sessionmaker(bind=engine, expire_on_commit=False)
-
 
 class Base(AsyncAttrs, DeclarativeBase):
     pass
-
 
 class User(Base):
     __tablename__ = 'users'
@@ -18,17 +14,16 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     tg_id = mapped_column(BigInteger)
 
+class Task(Base):
+    __tablename__ = 'tasks'
 
-    class Task(Base):
-        __tablename__ = 'tasks'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(128))
+    completed: Mapped[bool] = mapped_column(default=False)
+    user: Mapped[int] = mapped_column(ForeignKey('users.id', ondelete='CASCADE'))
 
-        id: Mapped[int] = mapped_column(primary_key=True)
-        title: Mapped[str] = mapped_column(String(128))
-        completed: Mapped[bool] = mapped_column(default=False)
-        user: Mapped[int] = mapped_column(ForeignKey('users.id', ondelete='CASCADE'))
-
-
-        async def init_db():
-            async with engine.begin() as conn:
-                await conn.run_sync(Base.metadata.create_all)
+# Переносим функцию init_db на уровень модуля
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
                 
